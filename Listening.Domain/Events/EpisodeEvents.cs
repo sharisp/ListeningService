fad54bee32019c395691d3e1f339fc0a325a4b1c
Andrew Wang <wthsir@gmail.com>
2025-09-30 12:21:04 +1000

    feat: episode event

---
+++ Listening.Domain/Events/EpisodeEvents.cs
@@ -0,0 +1,14 @@
+﻿using Listening.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listening.Domain.Events
+{
+    public record AddEpisodeEvent(Episode episode) : INotification;
+    public record UpdateEpisodeEvent(Episode episode) : INotification;
+    public record DeleteEpisodeEvent(Episode episode) : INotification;
+}
