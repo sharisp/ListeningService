77ad8a80292184c5c3cd6ddd830e5cf02c007f8b
Andrew Wang <wthsir@gmail.com>
2025-10-01 19:11:24 +1000

    feat: rabbitmq config

---
+++ Listening.Infrastructure/Options/RabbitMqConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listening.Infrastructure.Options
+{
+    
+    public class RabbitMqOptions
+    {
+        public string Host { get; set; } = "localhost";
+        public int Port { get; set; } = 5672;
+        public string UserName { get; set; } = "guest";
+        public string Password { get; set; } = "guest";
+        public string VirtualHost { get; set; } = "/";
+    }
+
+}
